f301d4efa6b080858fc687326c0571cbfd353c2e
c0utin <rafaelcouto111@gmail.com>
2025-04-07 14:30:20 -0300

    Add blockchain v1
    versao simples com pow

---
+++ blockchain-v1/src/main.rs
@@ -0,0 +1,8 @@
+mod consensus;
+
+fn main() {
+    let genesis_block = consensus::pow::Block::new(0, 0, "0", "Genesis Block", 10, 2);
+    let new_block = consensus::pow::generate_block_with_pow(&genesis_block, "Some data", 2);
+
+    println!("{}", new_block);
+}
